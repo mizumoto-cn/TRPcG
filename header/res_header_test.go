9ac579a6a7c36663c47558515b66f48ad500ab99
Mizumoto <mizumotokunn@gmail.com>
2022-06-23 10:36:50 +0900

    fixed unit_test

--- header/res_header_test.go
+++ header/res_header_test.go
@@ -11,7 +11,7 @@ import (
 // TestResponseHeader_Marshal tests ResponseHeader::Marshal
 func TestResponseHeader_Marshal(t *testing.T) {
 	header := &ResponseHeader{
-		CompressType: CompressType(compressor.Raw),
+		CompressType: compressor.Raw,
 		Error:        "error",
 		ID:           12345,
 		ResponseLen:  123,
@@ -37,7 +37,7 @@ func TestResponseHeader_Unmarshal(t *testing.T) {
 			[]byte{0x0, 0x0, 0xb9, 0x60, 0x5, 0x65, 0x72, 0x72, 0x6f, 0x72, 0x7b, 0x39, 0x30, 0x0, 0x0},
 			expect{
 				&ResponseHeader{
-					CompressType: CompressType(compressor.Raw),
+					CompressType: compressor.Raw,
 					Error:        "error",
 					ID:           12345,
 					ResponseLen:  123,
@@ -70,7 +70,7 @@ func TestResponseHeader_Unmarshal(t *testing.T) {
 // TestResponseHeader_ResetHeader tests ResponseHeader::ResetHeader
 func TestResponseHeader_ResetHeader(t *testing.T) {
 	header := &ResponseHeader{
-		CompressType: CompressType(compressor.Raw),
+		CompressType: compressor.Raw,
 		Error:        "error",
 		ID:           12345,
 		ResponseLen:  123,
@@ -83,7 +83,7 @@ func TestResponseHeader_ResetHeader(t *testing.T) {
 // TestResponseHeader_GetCompressType tests ResponseHeader::GetCompressType
 func TestResponseHeader_GetCompressType(t *testing.T) {
 	header := &ResponseHeader{
-		CompressType: CompressType(compressor.Raw),
+		CompressType: compressor.Raw,
 		Error:        "error",
 		ID:           12345,
 		ResponseLen:  123,
