f7ba9516bb4eee11964ceb8a060c79148aa14175
Mizumoto <mizumotokunn@gmail.com>
2023-10-15 12:08:24 +0900

    fix test

--- testing/json/test.svr.go
+++ testing/json/test.svr.go
@@ -1,7 +1,3 @@
-// Copyright 2022 <mzh.scnu@qq.com>. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package json
 
 import "errors"
