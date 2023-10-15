fe2c52c8797d22b5b9368f294d9051bf8e5aa343
Mizumoto <mizumotokunn@gmail.com>
2023-10-15 12:16:27 +0900

    new test service generated

--- testing/message/arith.svr.go
+++ testing/message/arith.svr.go
@@ -9,24 +9,28 @@ type ArithService struct{}
 
 // Add addition
 func (this *ArithService) Add(args *ArithRequest, reply *ArithResponse) error {
+	// define your service ...
 	reply.C = args.A + args.B
 	return nil
 }
 
 // Sub subtraction
 func (this *ArithService) Sub(args *ArithRequest, reply *ArithResponse) error {
+	// define your service ...
 	reply.C = args.A - args.B
 	return nil
 }
 
 // Mul multiplication
 func (this *ArithService) Mul(args *ArithRequest, reply *ArithResponse) error {
+	// define your service ...
 	reply.C = args.A * args.B
 	return nil
 }
 
 // Div division
 func (this *ArithService) Div(args *ArithRequest, reply *ArithResponse) error {
+	// define your service ...
 	if args.B == 0 {
 		return errors.New("divided by zero")
 	}
